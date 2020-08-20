fe01943d23f96f3158938e66f63118b60bd84de4
Austin McCalley <austin@austinmccalley.com>
2020-08-19 17:42:55 -0700

    MongoDB connect message

--- server/index.ts
+++ server/index.ts
@@ -8,22 +8,23 @@ import config from '../config'
 import discord from './utils/discord'
 
 import {
-  Props, 
+  Props,
 } from '@interfaces'
 
 
 const getDate = (): string => '[UTC] ' + new Date().toLocaleString('en-US', { timeZone: 'UTC' })
 
 const success = (...args: Array<string>): void => {
-  console.log(`${ chalk.bgGreen.black(' API ') } ${chalk.gray(getDate())} `, ...args, '\n')
+  console.log(`${chalk.bgGreen.black(' API ')} ${chalk.gray(getDate())} `, ...args, '\n')
 }
 
 const failure = (...args: Array<string>): void => {
-  console.log(`${ chalk.bgRed.black(' API ') } ${chalk.gray(getDate())} `, ...args, '\n')
+  console.log(`${chalk.bgRed.black(' API ')} ${chalk.gray(getDate())} `, ...args, '\n')
 }
 
 const init = async (): Promise<void> => {
   const dbClient = await MongoClient.connect(config.database.url, { useUnifiedTopology: true })
+  success('Connected to MongoDB')
   const db = dbClient.db(config.database.name)
 
   const props: Props = {
